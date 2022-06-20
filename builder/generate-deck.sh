53238c9f1e8ac272856f05d258ce9813a8517056
Eric Herman <eric@freesa.org>
2022-06-20 16:53:56 +0200

    builder to support newer Inkscape commandline options

--- builder/generate-deck.sh
+++ builder/generate-deck.sh
@@ -16,24 +16,60 @@ mkdir assets
 rm -rf outputdeck
 mkdir outputdeck
 
+INKSCAPE_VERSION=`inkscape --version | sed 's/Inkscape \([0-9\.]*\) .*/\1/'`
+INKSCAPE_MAJOR_VERSION=`echo "$INKSCAPE_VERSION" | cut -f1 -d'.'`
+INKSCAPE_MINOR_VERSION=`echo "$INKSCAPE_VERSION" | cut -f2 -d'.'`
+INKSCAPE_PATCH_VERSION=`echo "$INKSCAPE_VERSION" | cut -f3 -d'.'`
+if [ "_${VERBOSE}_" != "__" ] && [ $VERBOSE -gt 0 ]; then
+	cat <<END_OF_DOC
+INKSCAPE_VERSION: $INKSCAPE_VERSION
+INKSCAPE_MAJOR_VERSION: $INKSCAPE_MAJOR_VERSION
+INKSCAPE_MINOR_VERSION: $INKSCAPE_MINOR_VERSION
+INKSCAPE_PATCH_VERSION: $INKSCAPE_PATCH_VERSION
+END_OF_DOC
+fi
+
+function inkscape_convert_svg_to_png() {
+	svg_path=$1
+	png_path=assets/"$(basename -s .svg "$svg_path")".png
+	echo Converting image $svg_path to $png_path
+	if [ $INKSCAPE_MAJOR_VERSION -gt 0 ]; then
+		inkscape \
+			--export-area-drawing \
+			$svg_path \
+			--export-dpi=1200 \
+			--export-filename=$png_path
+	else
+		inkscape \
+			   --without-gui \
+			   --export-area-drawing \
+			   --export-dpi=1200 \
+			   --file=$svg_path \
+			   --export-png=$png_path \
+			   --export-png
+	fi
+	if [ "_${VERBOSE}_" != "__" ] && [ $VERBOSE -gt 0 ]; then
+		ls -l $png_path
+	fi
+	echo "[DONE]"
+}
 
 echo "Converting SVG image assets to LaTeX"
 
 for filename in ../assets/*.svg; do
-  echo Converting image $filename to PNG
-#inkscape -z -D --file=assets/"$(basename "$filename")".svg --export-pdf=assets/"$(basename -s .svg "$filename")".pdf --export-latex
- inkscape -z -D --file=../assets/"$(basename "$filename")" --export-dpi=1200 --export-png=assets/"$(basename -s .svg "$filename")".png --export-png
-  if [ "_${VERBOSE}_" != "__" ] && [ $VERBOSE -gt 0 ]; then
-    ls -l assets/"$(basename -s .svg "$filename")".png
-  fi
-  echo "[DONE]"
+	inkscape_convert_svg_to_png $filename
 done
 
 echo "Building cards..."
 
 for filename in ../cards/*.md; do
-  echo  Building card: outputdeck/"$(basename "$filename" .md)".pdf
-  pandoc --from=markdown+yaml_metadata_block --template card-front.tex -o outputdeck/"$(basename "$filename" .md)".pdf -V monofont="DejaVu Sans Mono" --pdf-engine=xelatex $filename
+	echo  Building card: outputdeck/"$(basename "$filename" .md)".pdf
+	pandoc \
+		--from=markdown+yaml_metadata_block \
+		--template card-front.tex \
+		-o outputdeck/"$(basename "$filename" .md)".pdf \
+		-V monofont="DejaVu Sans Mono" \
+		--pdf-engine=xelatex $filename
 done
 #  pandoc --from=markdown+yaml_metadata_block --template card.tex -o outputdeck/bug.pdf --pdf-engine=xelatex ../cards/bug.md
 #  -V mainfont="DejaVu Serif" \
